41da0a58c33f8ee20d95721ebfe928a3aae7b8e5
Marco Tizzoni <marco.tizzoni@gmail.com>
2014-10-20 14:33:17 +0200

    Parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,11 +8,37 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #
 class oracle_webgate (
-  $package_name = $oracle_webgate::params::package_name,
-  $service_name = $oracle_webgate::params::service_name,
+  $serverId          = $oracle_webgate::params::serverId,
+  $hostname          = $oracle_webgate::params::hostname,
+  $webgateId         = $oracle_webgate::params::webgateId,
+  $password          = $oracle_webgate::params::password,
+  $passphrase        = $oracle_webgate::params::passphrase,
+  $certFile          = $oracle_webgate::params::certFile,
+  $keyFile           = $oracle_webgate::params::keyFile,
+  $chainFile         = $oracle_webgate::params::chainFile,
+  $installLocation   = $oracle_webgate::params::installLocation,
+  $user              = $oracle_webgate::params::user,
+  $group             = $oracle_webgate::params::group,
+  $defaultLang       = $oracle_webgate::params::defaultLang,
+  $installLang       = $oracle_webgate::params::installLang,
+  $securityMode      = $oracle_webgate::params::securityMode
 ) inherits oracle_webgate::params {
 
   # validate parameters here
+  validate_string(serverId)
+  validate_string(hostname)
+  validate_string(webgateId)
+  validate_string(password)
+  validate_string(passphrase)
+  validate_absolute_path(certFile)
+  validate_absolute_path(keyFile)
+  validate_absolute_path(chainFile)
+  validate_absolute_path(installLocation)
+  validate_strin(user)
+  validate_string(group)
+  validate_string(defaultLang)
+  validate_string(installLang)
+  validate_string(group)
 
   class { 'oracle_webgate::install': } ->
   class { 'oracle_webgate::config': } ~>
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,15 +3,36 @@
 # This class is meant to be called from oracle_webgate
 # It sets variables according to platform
 #
-class oracle_webgate::params {
-  case $::osfamily {
-    'Debian': {
-      $package_name = 'oracle_webgate'
-      $service_name = 'oracle_webgate'
+class oracle_webgate::params (
+  $serverId          = undef,
+  $hostname          = undef,
+  $webgateId         = undef,
+  $password          = undef,
+  $passphrase        = undef,
+  $certFile          = undef,
+  $keyFile           = undef,
+  $chainFile         = undef,
+  $installLocation   = "/opt/netpoint/webgate/access",
+  $user              = "apache",
+  $group             = "apache",
+  $defaultLang       = "en-us",
+  $installLang       = "en-us",
+  $securityMode      = "cert",
+  $install           = "install",
+  $autoUpdate        = "No",
+  $launchBrowser     = "No",
+  )
+  {
+  case $::osfamily{
+    'RedHat' {
+      installLocation   => "/opt/netpoint/webgate/access",
+      user              => "apache",
+      group             => "apache",
     }
-    'RedHat', 'Amazon': {
-      $package_name = 'oracle_webgate'
-      $service_name = 'oracle_webgate'
+    'Debian' {
+        installLocation => "/opt/netpoint/webgate/access",
+        user            => "httpd",
+        group           => "httpd",
     }
     default: {
       fail("${::operatingsystem} not supported")
